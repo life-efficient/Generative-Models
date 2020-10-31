2a8e5a17bd73d653c23b930d5c99a786302a470f
Ubuntu <ubuntu@ip-172-31-38-9.eu-west-2.compute.internal>
2020-10-31 23:04:31 +0000

    working results

--- DCGAN.py
+++ DCGAN.py
@@ -4,7 +4,7 @@ from torch import nn
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-batch_size = 64
+batch_size = 128
 latent_vec_size = 128
 
 train_data = datasets.MNIST(
@@ -17,12 +17,12 @@ train_data = datasets.MNIST(
 train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
 
 #%%
-def sample(writer=None, device='cpu'):
+def sample(writer=None, device='cpu', tag='test'):
     if writer is None:
         writer = SummaryWriter(log_dir=f'runs/DCGAN/{time()}')
     z = torch.randn(batch_size, latent_vec_size).to(device)
     for img in G(z):
-        writer.add_image(f'test', img)
+        writer.add_image(f'DCGAN/{tag}', img)
 
 #%%
 
@@ -30,17 +30,14 @@ class Discriminator(nn.Module):
     def __init__(self):
         super().__init__()
         self.layers = nn.Sequential(
-            nn.Conv2d(1, 16, 3, 2),
-            nn.LeakyReLU(),
-            nn.BatchNorm2d(16),
-            nn.Conv2d(16, 64, 3, 2),
+            nn.Conv2d(1, 64, 4, 2, 1),
             nn.LeakyReLU(),
             nn.BatchNorm2d(64),
-            nn.Conv2d(64, 128, 3, 2),
+            nn.Conv2d(64, 128, 4, 2, 1),
             nn.LeakyReLU(),
             nn.BatchNorm2d(128),
             nn.Flatten(),
-            nn.Linear(128, 64),
+            nn.Linear(128*7*7, 64),
             nn.LeakyReLU(),
             nn.Linear(64, 1),
             nn.Sigmoid()
@@ -71,34 +68,47 @@ class Generator(nn.Module):
     def __init__(self):
         super().__init__()
         self.layers = nn.Sequential(
-            nn.Linear(128, 3*3*128),
-            nn.LeakyReLU(),
-            # nn.Linear(256, 16),
+            # nn.Linear(128, 3*3*64),
             # nn.LeakyReLU(),
-            Unflatten((128, 3, 3)),
+            # # nn.Linear(256, 16),
+            # # nn.LeakyReLU(),
+            # Unflatten((64, 3, 3)),
+            # nn.BatchNorm2d(64),
+            # nn.ConvTranspose2d(64, 64, 3, 2),
+            # nn.LeakyReLU(),
+            # nn.BatchNorm2d(64),
+            # nn.ConvTranspose2d(64, 16, 2, 2),
+            # nn.LeakyReLU(),
+            # nn.BatchNorm2d(16),
+            # nn.ConvTranspose2d(16, 1, 2, 2),
+            # nn.Sigmoid(),
+
+            nn.Linear(128, 256),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(256),
+            nn.Linear(256, 1024),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(1024),
+            nn.Linear(1024, 128*7*7),
+            nn.LeakyReLU(),
+            Unflatten((128, 7, 7)),
             nn.BatchNorm2d(128),
-            nn.ConvTranspose2d(128, 64, 3, 2),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1), #128x7x7 -> 64x14x14
             nn.LeakyReLU(),
             nn.BatchNorm2d(64),
-            nn.ConvTranspose2d(64, 16, 2, 2),
-            nn.LeakyReLU(),
-            nn.BatchNorm2d(16),
-            nn.ConvTranspose2d(16, 1, 2, 2),
-            nn.Sigmoid(),
+            nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1), #64x14x14 -> 1x28x28
+            nn.Sigmoid()
         )
 
     def forward(self, z):
-        print('generating')
-        print(z.shape)
         z = self.layers(z)
-        print(z.shape)
         return z
 
 g = Generator()
 ran_batch = torch.rand(batch_size, latent_vec_size)
 fake = g(ran_batch)
 print('generated shape:', fake.shape)
-sdfs
+# sdfs
 
 #%%
 def show(x):
@@ -109,15 +119,13 @@ def show(x):
 from torch.utils.tensorboard import SummaryWriter
 from time import time
 
-toPILImg = transforms.ToPILImage()
-
 criterion = nn.BCELoss()
 
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 def train(G, D, epochs=1):
     optimiser_d = torch.optim.Adam(D.parameters(), lr=0.0001)
-    optimiser_g = torch.optim.Adam(G.parameters(), lr=0.0001)
+    optimiser_g = torch.optim.Adam(G.parameters(), lr=0.001)
     writer = SummaryWriter(log_dir=f'runs/DCGAN/{time()}')
     G = G.to(device)
     D = D.to(device)
@@ -132,23 +140,19 @@ def train(G, D, epochs=1):
             # GENERATOR UPDATE
             optimiser_g.zero_grad()
             labels = torch.ones(batch_size).to(device)
-            print(labels.shape)
-            print(D(G(z)).shape)
             G_loss = criterion(D(G(z)), labels)
             # G_loss = - torch.log(1 - D(G(z)))
             # G_loss = torch.mean(G_loss)
             G_loss.backward(retain_graph=True)
             optimiser_g.step()
 
-            print('updating d')
             # DISCRIMINATOR UPDATE
             optimiser_d.zero_grad()
             labels = torch.zeros(batch_size).to(device)
-            D_loss = criterion(D(G(z)), labels) # loss on fake examples
+            D_loss = criterion(D(G(z).detach()), labels) # loss on fake examples
             # D_loss = - (torch.log(D(x)) + torch.log(1 - D(G(z))))
             # D_loss = torch.mean(D_loss)
             D_loss.backward()
-            print('\treal')
             labels = torch.ones(x.shape[0]).to(device)
             D_loss = criterion(D(x), labels)
             D_loss.backward()
@@ -165,7 +169,7 @@ def train(G, D, epochs=1):
             )
             if idx % 100 == 0:
                 print('sampling')
-                sample(writer, device)
+                sample(writer, device, tag=f'{time()}')
 
 G = Generator()
 D = Discriminator()
