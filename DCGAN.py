45ef01df453dac2e6385cd5794e0f62689edf8ee
Ubuntu <ubuntu@ip-172-31-38-9.eu-west-2.compute.internal>
2020-10-31 22:28:06 +0000

    fixed shapes

--- DCGAN.py
+++ DCGAN.py
@@ -4,7 +4,8 @@ from torch import nn
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-batch_size=64
+batch_size = 64
+latent_vec_size = 128
 
 train_data = datasets.MNIST(
     train=True,
@@ -35,21 +36,26 @@ class Discriminator(nn.Module):
             nn.Conv2d(16, 64, 3, 2),
             nn.LeakyReLU(),
             nn.BatchNorm2d(64),
-            nn.Flatten(),
-            nn.Linear(2304, 256),
+            nn.Conv2d(64, 128, 3, 2),
             nn.LeakyReLU(),
-            nn.Linear(256, 1),
+            nn.BatchNorm2d(128),
+            nn.Flatten(),
+            nn.Linear(128, 64),
+            nn.LeakyReLU(),
+            nn.Linear(64, 1),
             nn.Sigmoid()
         )
     
     def forward(self, x):
-        return self.layers(x)
+        x = self.layers(x)
+        # print('discriminator output:', x.shape)
+        return x
 
 d = Discriminator()
 
-x, _ = train_data[0]
-x = x.unsqueeze(0)
-print(d(x))
+# x, _ = train_data[0]
+# x = x.unsqueeze(0)
+# print(d(x))
 
 # %%
 
@@ -59,35 +65,40 @@ class Unflatten(nn.Module):
         self.out_shape = out_shape
 
     def forward(self, x):
-        return x.view(-1, *self.out_shape)
+        return x.view(x.shape[0], *self.out_shape)
 
 class Generator(nn.Module):
     def __init__(self):
         super().__init__()
         self.layers = nn.Sequential(
-            nn.Linear(100, 256),
+            nn.Linear(128, 3*3*128),
             nn.LeakyReLU(),
-            nn.Linear(256, 1152),
+            # nn.Linear(256, 16),
             # nn.LeakyReLU(),
-            # nn.Linear(1024, 1152),
-            nn.LeakyReLU(),
             Unflatten((128, 3, 3)),
-            nn.ConvTranspose2d(128, 64, 3, 3),
+            nn.BatchNorm2d(128),
+            nn.ConvTranspose2d(128, 64, 3, 2),
             nn.LeakyReLU(),
-            nn.ConvTranspose2d(64, 32, 3, 3),
+            nn.BatchNorm2d(64),
+            nn.ConvTranspose2d(64, 16, 2, 2),
             nn.LeakyReLU(),
-            nn.ConvTranspose2d(32, 1, 2, 1),
+            nn.BatchNorm2d(16),
+            nn.ConvTranspose2d(16, 1, 2, 2),
             nn.Sigmoid(),
         )
 
     def forward(self, z):
-        return self.layers(z)
+        print('generating')
+        print(z.shape)
+        z = self.layers(z)
+        print(z.shape)
+        return z
 
 g = Generator()
-latent_vec_size = 100
 ran_batch = torch.rand(batch_size, latent_vec_size)
 fake = g(ran_batch)
-print(fake.shape)
+print('generated shape:', fake.shape)
+sdfs
 
 #%%
 def show(x):
@@ -105,7 +116,7 @@ criterion = nn.BCELoss()
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 def train(G, D, epochs=1):
-    optimiser_d = torch.optim.Adam(D.parameters(), lr=0.00001)
+    optimiser_d = torch.optim.Adam(D.parameters(), lr=0.0001)
     optimiser_g = torch.optim.Adam(G.parameters(), lr=0.0001)
     writer = SummaryWriter(log_dir=f'runs/DCGAN/{time()}')
     G = G.to(device)
@@ -121,12 +132,15 @@ def train(G, D, epochs=1):
             # GENERATOR UPDATE
             optimiser_g.zero_grad()
             labels = torch.ones(batch_size).to(device)
+            print(labels.shape)
+            print(D(G(z)).shape)
             G_loss = criterion(D(G(z)), labels)
             # G_loss = - torch.log(1 - D(G(z)))
             # G_loss = torch.mean(G_loss)
             G_loss.backward(retain_graph=True)
             optimiser_g.step()
 
+            print('updating d')
             # DISCRIMINATOR UPDATE
             optimiser_d.zero_grad()
             labels = torch.zeros(batch_size).to(device)
@@ -134,6 +148,7 @@ def train(G, D, epochs=1):
             # D_loss = - (torch.log(D(x)) + torch.log(1 - D(G(z))))
             # D_loss = torch.mean(D_loss)
             D_loss.backward()
+            print('\treal')
             labels = torch.ones(x.shape[0]).to(device)
             D_loss = criterion(D(x), labels)
             D_loss.backward()
